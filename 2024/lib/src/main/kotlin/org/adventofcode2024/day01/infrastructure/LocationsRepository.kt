159e606cd5123218e72891393f695b319a5b6e4c
Daniel Ruiz <danruicar@gmail.com>
2025-01-06 00:38:50 +0100

    Add `LocationsRepository` class to retrieve locations from a file reader

---
+++ 2024/lib/src/main/kotlin/org/adventofcode2024/day01/infrastructure/LocationsRepository.kt
@@ -0,0 +1,20 @@
+package adventofcode2024
+import java.io.Reader
+
+class LocationsRepository {
+  fun getFromReader(reader: Reader): Locations {
+    var right: MutableList<Int> = mutableListOf()
+    var left: MutableList<Int> = mutableListOf()
+    val separator = "   "
+
+    for (line in reader.readLines()) {
+      val values = line.split(separator)
+      val rightLocation = values.first().toInt()
+      right.add(rightLocation)
+      val leftLocation = values.last().toInt()
+      left.add(leftLocation)
+    }
+    
+    return Locations(right, left)
+  }
+}
---
+++ 2024/lib/src/test/kotlin/org/adventofcode2024/day01/infrastructure/LocationsRepositoryTest.kt
@@ -0,0 +1,15 @@
+package adventofcode2024
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LocationsRepositoryTest {
+  @Test
+  fun `Should return locations from an input stream reader`() {
+    val reader = "3   4\n4   3".reader()
+    val expectedLocations = Locations(rightList=listOf(3, 4), leftList=listOf(4, 3))
+
+    val locations = LocationsRepository().getFromReader(reader)
+
+    assertEquals(locations, expectedLocations)
+  }
+}
